e37b23e7500d10f027480276d59694b4ef0ad8a7
hengtseChou <hankthedev@gmail.com>
2024-10-26 22:11:08 +0800

    misc: formation

--- Standalone/setup.sh
+++ Standalone/setup.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 if ! command -v paru 2>&1 >/dev/null; then
-    echo ":: Error: paru is not installed. Exiting."
-    exit 1
+	echo ":: Error: paru is not installed. Exiting."
+	exit 1
 fi
 
 utils=(
-    btop
-    github-cli
-    go
-    gotop-bin
-    htop
-    ifuse
-    man-db
-    ncdu
-    npm
-    rust
-    speech-dispatcher
-    trash-cli
+	btop
+	github-cli
+	go
+	gotop-bin
+	htop
+	ifuse
+	man-db
+	ncdu
+	npm
+	rust
+	speech-dispatcher
+	trash-cli
 )
 
 apps=(
-    btrfs-assistant
-    chromium
-    docker
-    firefox
-    onefetch
-    onlyoffice-bin
-    peaclock
-    rclone
-    spotify
-    timeshift
-    visual-studio-code-bin
-    vlc-git
-    zotero-bin
+	btrfs-assistant
+	chromium
+	docker
+	firefox
+	onefetch
+	onlyoffice-bin
+	peaclock
+	rclone
+	spotify
+	timeshift
+	visual-studio-code-bin
+	vlc-git
+	zotero-bin
 )
 
 echo ":: Installing utils..."
@@ -43,42 +43,42 @@ paru -S --needed "${apps[@]}"
 read -p ":: Install fcitx5? (Y/n): " install_fcitx5
 install_fcitx5=${install_fcitx5:-Y}
 if [[ "$install_fcitx5" =~ ^([yY][eE][sS]?|[yY])$ ]]; then
-    paru -S --needed fcitx5-im fcitx5-chewing fcitx5-mcbopomofo-git
-    sudo update-icon-caches /usr/share/icons/*
-    echo ":: Configuring fcitx5..."
-    echo ":: Note that this configuration is for GTK + Wayland."
-    if [ -f ~/.gtkrc-2.0 ]; then
-        echo "gtk-im-module="fcitx"" >>~/.gtkrc-2.0
-    else
-        echo "gtk-im-module="fcitx"" >~/.gtkrc-2.0
-    fi
-    if [ -f ~/.config/gtk-3.0/settings.ini ]; then
-        echo "gtk-im-module=fcitx" >>~/.config/gtk-3.0/settings.ini
-    else
-        echo "[Settings]" >~/.config/gtk-3.0/settings.ini
-        echo "gtk-im-module=fcitx" >>~/.config/gtk-3.0/settings.ini
-    fi
-    if [ -f ~/.config/gtk-4.0/settings.ini ]; then
-        echo "gtk-im-module=fcitx" >>~/.config/gtk-4.0/settings.ini
-    else
-        echo "[Settings]" >~/.config/gtk-4.0/settings.ini
-        echo "gtk-im-module=fcitx" >>~/.config/gtk-4.0/settings.ini
-    fi
-    gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/IMModule':<'fcitx'>}"
+	paru -S --needed fcitx5-im fcitx5-chewing fcitx5-mcbopomofo-git
+	sudo update-icon-caches /usr/share/icons/*
+	echo ":: Configuring fcitx5..."
+	echo ":: Note that this configuration is for GTK + Wayland."
+	if [ -f ~/.gtkrc-2.0 ]; then
+		echo "gtk-im-module="fcitx"" >>~/.gtkrc-2.0
+	else
+		echo "gtk-im-module="fcitx"" >~/.gtkrc-2.0
+	fi
+	if [ -f ~/.config/gtk-3.0/settings.ini ]; then
+		echo "gtk-im-module=fcitx" >>~/.config/gtk-3.0/settings.ini
+	else
+		echo "[Settings]" >~/.config/gtk-3.0/settings.ini
+		echo "gtk-im-module=fcitx" >>~/.config/gtk-3.0/settings.ini
+	fi
+	if [ -f ~/.config/gtk-4.0/settings.ini ]; then
+		echo "gtk-im-module=fcitx" >>~/.config/gtk-4.0/settings.ini
+	else
+		echo "[Settings]" >~/.config/gtk-4.0/settings.ini
+		echo "gtk-im-module=fcitx" >>~/.config/gtk-4.0/settings.ini
+	fi
+	gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/IMModule':<'fcitx'>}"
 else
-    echo ":: Skipping fcitx5 installation."
+	echo ":: Skipping fcitx5 installation."
 fi
 
 read -p ":: Install R and RStudio? (Y/n): " install_r
 install_r=${install_r:-Y}
 if [[ "$install_r" =~ ^([yY][eE][sS]?|[yY])$ ]]; then
-    paru -S --needed r gcc-fortran
-    # install RStudio with the gist PKGBUILD
-    git clone https://gist.github.com/9bad76d97ff17e37980cf40416fc5596.git rstudio-desktop-bin
-    cd rstudio-desktop-bin
-    makepkg -si
-    cd ..
-    rm -rf rstudio-desktop-bin
+	paru -S --needed r gcc-fortran
+	# install RStudio with the gist PKGBUILD
+	git clone https://gist.github.com/9bad76d97ff17e37980cf40416fc5596.git rstudio-desktop-bin
+	cd rstudio-desktop-bin
+	makepkg -si
+	cd ..
+	rm -rf rstudio-desktop-bin
 else
-    echo ":: Skipping R and RStudio installation."
+	echo ":: Skipping R and RStudio installation."
 fi
--- install.sh
+++ install.sh
@@ -5,42 +5,42 @@ conf=true
 scripts=true
 
 _help() {
-    echo "Usage: $0 [options]"
-    echo "Options:"
-    echo "  --skip-gnome: Skip GNOME apps and settings"
-    echo "  --skip-hypr: Skip Hyprland apps and settings"
-    echo "  --skip-conf: Skip app config files"
-    echo "  --skip-scripts: Skip cloning scripts"
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  --skip-gnome: Skip GNOME apps and settings"
+  echo "  --skip-hypr: Skip Hyprland apps and settings"
+  echo "  --skip-conf: Skip app config files"
+  echo "  --skip-scripts: Skip cloning scripts"
 }
 
 for arg in "$@"; do
-    case $arg in
-    --skip-gnome)
-        gnome=false
-        shift
-        ;;
-    --skip-hypr)
-        hypr=false
-        shift
-        ;;
-    --skip-conf)
-        conf=false
-        shift
-        ;;
-    --skip-scripts)
-        scripts=false
-        shift
-        ;;
-    --help)
-        _help
-        exit 0
-        ;;
-    *)
-        echo "Unknown option: $arg"
-        _help
-        exit 1
-        ;;
-    esac
+  case $arg in
+  --skip-gnome)
+    gnome=false
+    shift
+    ;;
+  --skip-hypr)
+    hypr=false
+    shift
+    ;;
+  --skip-conf)
+    conf=false
+    shift
+    ;;
+  --skip-scripts)
+    scripts=false
+    shift
+    ;;
+  --help)
+    _help
+    exit 0
+    ;;
+  *)
+    echo "Unknown option: $arg"
+    _help
+    exit 1
+    ;;
+  esac
 done
 
 # install aur helper
@@ -52,25 +52,25 @@ cd ..
 rm -rf paru
 
 if $gnome; then
-    echo ":: Setting up GNOME..."
-    ./Gnome/setup.sh
-    echo ":: Done."
+  echo ":: Setting up GNOME..."
+  ./Gnome/setup.sh
+  echo ":: Done."
 fi
 if $hypr; then
-    echo ":: Setting up Hyprland..."
-    ./Hypr/setup.sh
-    echo ":: Done."
+  echo ":: Setting up Hyprland..."
+  ./Hypr/setup.sh
+  echo ":: Done."
 fi
 echo ":: Installing standalone apps..."
 ./Standalone/setup.sh
 echo ":: Done."
 if $conf; then
-    echo ":: Applying app config files..."
-    ./Conf/setup.sh
-    echo ":: Done."
+  echo ":: Applying app config files..."
+  ./Conf/setup.sh
+  echo ":: Done."
 fi
 if $scripts; then
-    echo ":: Cloning scripts into Home folder..."
-    git clone https://github.com/hengtseChou/Scripts.git ~/Scripts
-    echo ":: Done."
+  echo ":: Cloning scripts into Home folder..."
+  git clone https://github.com/hengtseChou/Scripts.git ~/Scripts
+  echo ":: Done."
 fi
