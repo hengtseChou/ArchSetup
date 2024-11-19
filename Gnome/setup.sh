a023cf03b9e80c9ef13d74f4167e1fefb11106f6
hengtseChou <hankthedev@gmail.com>
2024-11-19 14:17:12 +0800

    change: improved gnome setup script

--- Gnome/setup.sh
+++ Gnome/setup.sh
@@ -1,79 +1,95 @@
 #!/bin/bash
 apps=(
-	dconf-editor
-	file-roller
-	font-manager
-	gnome-browser-connector
-	gnome-calculator
-	gnome-control-center
-	gnome-logs
-	gnome-menus
-	gnome-screenshot
-	gnome-shell
-	gnome-terminal
-	gnome-text-editor
-	gthumb
-	gvfs-google
-	loupe
-	menulibre
-	nautilus
-	polkit-gnome
-	seahorse
-	sushi
-	xdg-user-dirs-gtk
+  dconf-editor
+  file-roller
+  font-manager
+  gnome-browser-connector
+  gnome-calculator
+  gnome-control-center
+  gnome-logs
+  gnome-menus
+  gnome-screenshot
+  gnome-shell
+  gnome-terminal
+  gnome-text-editor
+  gthumb
+  gvfs-google
+  loupe
+  menulibre
+  nautilus
+  polkit-gnome
+  seahorse
+  sushi
+  xdg-user-dirs-gtk
 )
 
 theming=(
-	yaru-gtk-theme
-	yaru-icon-theme
+  yaru-gtk-theme
+  yaru-icon-theme
 )
 
 fonts=(
-	ttf-ubuntu-font-family
-	ttf-ubuntu-mono-nerd
+  ttf-ubuntu-font-family
+  ttf-ubuntu-mono-nerd
 )
-
-echo ":: Installing apps..."
-paru -S --needed "${apps[@]}"
-echo ":: Installing fonts..."
-paru -S --needed "${fonts[@]}"
-read -p ":: Skip theming? (y/N): " skip_theming
-skip_theming=${skip_theming:-N}
-if [[ "$skip_theming" =~ ^([nN][oO]?|[yY][eE][sS]?)$ ]]; then
-	echo ":: Installing theme..."
-	paru -S --needed "${theming[@]}"
-	gsettings set org.gnome.desktop.interface gtk-theme 'Yaru'
-	gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
-else
-	echo ":: Skipping theme installation."
-fi
-echo ":: Restoring base settings..."
-dconf load / <$PWD/base-settings.ini
 
 extensions=(
-	AlphabeticalAppGrid@stuarthayhurst
-	blur-my-shell@aunetx
-	burn-my-windows@schneegans.github.com
-	caffeine@patapon.info
-	clipboard-indicator@tudmotu.com
-	dash-to-dock@micxgx.gmail.com
-	grand-theft-focus@zalckos.github.com
-	kimpanel@kde.org
+  AlphabeticalAppGrid@stuarthayhurst
+  blur-my-shell@aunetx
+  burn-my-windows@schneegans.github.com
+  caffeine@patapon.info
+  clipboard-indicator@tudmotu.com
+  dash-to-dock@micxgx.gmail.com
+  grand-theft-focus@zalckos.github.com
+  kimpanel@kde.org
 )
+
+echo -e "\n----- GNOME configuration script -----\n"
+echo ":: Installing apps..."
+paru -S --needed "${apps[@]}"
+echo -e ":: Done. Proceeding to the next step...\n"
+sleep 3
+
+echo ":: Installing fonts..."
+paru -S --needed "${fonts[@]}"
+echo -e ":: Done. Proceeding to the next step...\n"
+sleep 3
+
+read -p ":: Skip theming? (y/N): " skip_theming
+skip_theming=${skip_theming:-N}
+if [[ "$skip_theming" =~ ^([yY])$ ]]; then
+  echo ":: Skipping theme installation"
+  echo -e ":: Proceeding to the next step...\n"
+  sleep 3
+else
+  echo ":: Installing theme..."
+  paru -S --needed "${theming[@]}"
+  gsettings set org.gnome.desktop.interface gtk-theme 'Yaru'
+  gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
+  echo -e ":: Done. Proceeding to the next step...\n"
+  sleep 3
+fi
+
+echo ":: Restoring base settings..."
+dconf load / <$PWD/base-settings.ini
+echo -e ":: Done. Proceeding to the next step...\n"
+sleep 3
 
 read -p ":: Skip extensions install? (y/N): " skip_extensions
 skip_extensions=${skip_extensions:-N}
-if [[ "$skip_extensions" =~ ^([nN][oO]?|[yY][eE][sS]?)$ ]]; then
-	sudo pacman -S --needed python-pipx --noconfirm
-	pipx ensurepath
-	pipx install gnome-extensions-cli
-	echo ":: Installing extensions..."
-	for extension in "${extensions[@]}"; do
-		gext install $extension
-	done
-	echo ":: Restoring extensions settings..."
-	dconf load / <$PWD/extensions-settings.ini
-	fi
+if [[ "$skip_extensions" =~ ^([yY])$ ]]; then
+  echo -e ":: Skipping extensions install\n"
 else
-	echo ":: Skipping extensions install."
+  sudo pacman -S --needed python-pipx
+  pipx ensurepath
+  pipx install gnome-extensions-cli
+  echo ":: Installing extensions..."
+  for extension in "${extensions[@]}"; do
+    gext install $extension
+  done
+  echo ":: Restoring extensions settings..."
+  dconf load / <$PWD/extensions-settings.ini
+  echo -e ":: Done.\n"
 fi
+
+echo -e ":: GNOME configuration completed. "
