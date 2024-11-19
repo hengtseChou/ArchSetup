020520afafd1d491ac9d93094c6cd48370874234
hengtseChou <hankthedev@gmail.com>
2024-11-19 17:04:40 +0800

    change: improved setup script

--- Gnome/setup.sh
+++ Gnome/setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+if ! command -v paru 2>&1 >/dev/null; then
+  echo ":: Error: paru is not installed. Exiting."
+  exit 1
+fi
+
 apps=(
+  fastfetch
+  fontconfig
+  starship
+  zsh
+)
+
+gnome_apps=(
   dconf-editor
   file-roller
   font-manager
@@ -20,7 +32,17 @@ apps=(
   polkit-gnome
   seahorse
   sushi
+)
+
+utils=(
+  eza
+  fzf
+  ifuse
+  networkmanager
+  power-profiles-daemon
+  udiskie
   xdg-user-dirs-gtk
+  zoxide
 )
 
 theming=(
@@ -48,12 +70,27 @@ echo -e "\n----- GNOME configuration script -----\n"
 echo ":: Installing apps..."
 paru -S --needed "${apps[@]}"
 echo -e ":: Done. Proceeding to the next step...\n"
-sleep 3
+
+echo ":: Installing GNOME apps..."
+paru -S --needed "${gnome_apps[@]}"
+echo -e ":: Done. Proceeding to the next step...\n"
+
+echo ":: Installing utils..."
+paru -S --needed "${utils[@]}"
+echo -e ":: Done. Proceeding to the next step...\n"
 
 echo ":: Installing fonts..."
 paru -S --needed "${fonts[@]}"
 echo -e ":: Done. Proceeding to the next step...\n"
-sleep 3
+
+if [[ "$SHELL" != "/bin/zsh" ]]; then
+  echo ":: Setting up default shell..."
+  chsh -s /bin/zsh
+  echo -e ":: Done. Proceeding to the next step...\n"
+else
+  echo ":: Setting up default shell: Already set to zsh"
+  echo -e ":: Proceeding to the next step...\n"
+fi
 
 read -p ":: Skip theming? (y/N): " skip_theming
 skip_theming=${skip_theming:-N}
@@ -72,7 +109,6 @@ sleep 3
 echo ":: Restoring base settings..."
 dconf load / <$PWD/base-settings.ini
 echo -e ":: Done. Proceeding to the next step...\n"
-sleep 3
 
 read -p ":: Skip extensions installation? (y/N): " skip_extensions
 skip_extensions=${skip_extensions:-N}
@@ -88,7 +124,21 @@ else
   done
   echo ":: Restoring extensions settings..."
   dconf load / <$PWD/extensions-settings.ini
-  echo -e ":: Done."
+  echo ":: Done."
 fi
+
+echo ":: Setting up configuration files..."
+echo "" && sleep 0.5
+sudo systemctl enable greetd.service
+sudo cp ./greetd/config.toml /etc/greetd/config.toml
+echo ":: Copied $PWD/greetd/config.toml to /etc/greetd/config.toml"
+echo "" && sleep 0.5
+
+./symlink.sh $PWD/starship/starship.toml --to-config
+echo "" && sleep 0.5
+./symlink.sh $PWD/zsh --to-config
+echo "" && sleep 0.5
+./symlink.sh $PWD/zsh/.zshrc --to-home
+echo "" && sleep 0.5
 
 echo -e "\n:: GNOME configuration completed. \n"
